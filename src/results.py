6aa8fbf4cc309ee2e3d6e175ad48d3fdcd42564b
ozanarkancan <can.ozanarkan@gmail.com>
2015-10-12 16:02:00 +0300

    removed logs

--- src/results.py
+++ src/results.py
@@ -3,13 +3,15 @@ from utils import ROOT_DIR
 from os import walk
 import re
 import pandas as pd
+import sys
 
 LOG_DIR = '{}/logs'.format(ROOT_DIR)
 
 def get_arg_parser():
     parser = argparse.ArgumentParser(prog="results")
 
-    parser.add_argument("--job", default='collect', choices=['collect'], help= "job choice")
+    parser.add_argument("--job", default='collect',
+        choices=['collect','lang_results'], help= "job choice")
     
     return parser
 
@@ -39,7 +41,10 @@ def get_lang(lines):
     return lang
 
 def collect():
-    all_results = pd.DataFrame(columns=['file_name', 'lang', 'trn_best', 'dev_best', 'trn_logs', 'dev_logs'])
+    try:
+        all_results = pd.load('all_results.pkl')
+    except:
+        all_results = pd.DataFrame(columns=['file_name', 'lang', 'trn_best', 'dev_best', 'trn_logs', 'dev_logs'])
 
     files = []
     for (_, _, fnames) in walk(LOG_DIR):
@@ -47,29 +52,39 @@ def collect():
 
     files = filter(lambda s: s.endswith("info"), files)
 
-    i = 0
-    
+    i = all_results.shape[0]
+    indx = 0
     for fname in files:
-        with open(LOG_DIR + "/" + files[0], 'r') as f:
+        print "Processing: {} / {}\r".format(indx, len(files)),
+        sys.stdout.flush()
+        indx += 1
+        if fname in all_results['file_name'].values.tolist():
+            break
+        
+        with open(LOG_DIR + "/" + fname, 'r') as f:
+            try:
+                lines = f.readlines()
+                trn_lines = get_lines(lines, "trn")
+                dev_lines = get_lines(lines, "dev")
             
-            lines = f.readlines()
-            trn_lines = get_lines(lines, "trn")
-            dev_lines = get_lines(lines, "dev")
+                trn_df = get_experiment_frame(trn_lines)
+                dev_df = get_experiment_frame(dev_lines)
             
-            trn_df = get_experiment_frame(trn_lines)
-            dev_df = get_experiment_frame(dev_lines)
-            
-            all_results.loc[i] = [fname, get_lang(lines), trn_df['best'].max(),
-                dev_df['best'].max(), trn_df, dev_df]
+                all_results.loc[i] = [fname, get_lang(lines), trn_df['best'].max(),
+                    dev_df['best'].max(), trn_df, dev_df]
 
-            i += 1
-    print all_results
+                i += 1
+            except:
+                print "Error during processing file: " + fname
+
+    all_results.save('all_results.pkl')
+    return all_results
 
 if __name__ == "__main__":
     parser = get_arg_parser()
     args = vars(parser.parse_args())
+    df = collect()
 
-    if args['job'] == 'collect':
-        collect()
-    
-
+    if args['job'] == "lang_results":
+        g = df.groupby('lang')
+        print g['dev_best'].max()
