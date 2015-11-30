c21d2b9324d4b47bb1246474df48bfae0b04eb6b
ozanarkancan <can.ozanarkan@gmail.com>
2015-11-30 08:30:02 +0200

    modified hyperopt

--- src/exper_hyperopt.py
+++ src/exper_hyperopt.py
@@ -41,7 +41,7 @@ def get_arg_parser():
     parser.add_argument("--norm", default=5, type=float, help="Threshold for clipping norm of gradient")
     parser.add_argument("--n_batch", default=32, type=int, help="batch size")
     parser.add_argument("--fepoch", default=50, type=int, help="number of epochs")
-    parser.add_argument("--patience", default=-1, type=int, help="how patient the validator is")
+    parser.add_argument("--patience", default=5, type=int, help="how patient the validator is")
     parser.add_argument("--sample", default=0, type=int, help="num of sents to sample from trn in the order of K")
     parser.add_argument("--feat", default='basic', help="feat func to use")
     parser.add_argument("--emb", default=0, type=int, help="embedding layer size")
@@ -204,150 +204,13 @@ class Validator(object):
                 logging.info('old lr: {}, new lr: {}'.format(val, val * 0.95))
                 rdnn.lr.set_value(val * 0.95)
                 anger = 0
+                break
             logging.info('')
         return dbests['dev'][1]
-
-class Curriculum(object):
-    def __init__(self, trn, dev, batcher, reporter, numofparts):
-        self.trn = trn
-        self.dev = dev
-        self.trndat = batcher.get_batches(trn)
-        self.devdat = batcher.get_batches(dev)
-        self.batcher = batcher
-        self.reporter = reporter
-        self.numofparts = numofparts
-        self.trn_parts = []
-        sentineachpart = len(self.trn) / self.numofparts
-        
-        for i in xrange(self.numofparts):
-            l = i * sentineachpart
-            u = (i + 1) * sentineachpart if i != (self.numofparts - 1) else len(self.trn)
-            self.trn_parts.append(self.trn[l:u])
-
-        self.trn_parts.append(self.trn)
-
-    def validate(self, rdnn, fepoch, patience=-1):
-        for i in xrange(self.numofparts + 1):
-            logging.info('Learning part:{}'.format(i + 1))
-            validator = Validator(self.trn_parts[i], self.dev, self.batcher, self.reporter)
-            validator.validate(rdnn, fepoch, patience)
-        
-            
 
 LPARAMS = ['activation', 'n_hidden', 'fbmerge', 'drates',
     'recout','decoder', 'opt','lr','norm','gclip','truncate','n_batch', 'shuf',
     'breaktrn', 'captrn', 'emb','lang', 'reverse','tagging', 'fbias']
-
-def main():
-    parser = get_arg_parser()
-    args = vars(parser.parse_args())
-    args['drates'] = args['drates'] if any(args['drates']) else [0]*(len(args['n_hidden'])+1)
-
-    if args['rnn'] == 'nerrnn':
-        args['n_batch'] = 1
-
-    # logger setup
-    import socket
-    host = socket.gethostname().split('.')[0]
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    shandler = logging.StreamHandler()
-    shandler.setLevel(logging.INFO)
-    param_log_name = ','.join(['{}:{}'.format(p,args[p]) for p in LPARAMS])
-    param_log_name = valid_file_name(param_log_name)
-    base_log_name = '{}:{},{}'.format(host, theano.config.device, param_log_name if args['log'] == 'das_auto' else args['log'])
-    ihandler = logging.FileHandler('{}/{}.info'.format(LOG_DIR,base_log_name), mode='w')
-    ihandler.setLevel(logging.INFO)
-    dhandler = logging.FileHandler('{}/{}.debug'.format(LOG_DIR,base_log_name), mode='w')
-    dhandler.setLevel(logging.DEBUG)
-    logger.addHandler(shandler);logger.addHandler(ihandler);logger.addHandler(dhandler);
-    # end logger setup
-
-    # print args
-    for k,v in sorted(args.iteritems()):
-        logger.info('{}:\t{}'.format(k,v))
-    logger.info('{}:\t{}'.format('base_log_name',base_log_name))
-
-    trn, dev, tst = get_sents(args['lang'])
-
-    if args['breaktrn']:
-        trn = [subsent for sent in trn for subsent in break2subsents(sent)]
-
-    if args['captrn']:
-        trn = filter(lambda sent: len(sent['ws'])<args['captrn'], trn)
-
-    if args['sample']>0:
-        trn_size = args['sample']*1000
-        trn = sample_sents(trn,trn_size)
-
-    repclass = getattr(rep, 'Rep'+args['rep'])
-    repobj = repclass()
-
-    for d in (trn,dev,tst):
-        for sent in d:
-            if args['tagging'] == 'io':
-                sent['ts'] = encoding.any2io(sent['ts'])
-            sent.update({
-                'cseq': repobj.get_cseq(sent), 
-                'wiseq': repobj.get_wiseq(sent), 
-                'tseq': repobj.get_tseq(sent)})
-    
-    if args['reverse']:
-        def reverse_data(d):
-            copy_d = d.copy()
-            
-            for k in copy_d.keys():
-                if k == "wiseq":
-                    m = max(copy_d[k])
-                    copy_d[k] = map(lambda x: -1 if x == -1 else m - x, copy_d[k][::-1])
-                else:
-                    copy_d[k] = copy_d[k][::-1]
-            return copy_d
-
-        reversed = map(reverse_data, trn)
-        trn += reversed
-
-
-    if args['sorted']:
-        trn = sorted(trn, key=lambda sent: len(sent['cseq']))
-        dev = sorted(dev, key=lambda sent: len(sent['cseq']))
-        tst = sorted(tst, key=lambda sent: len(sent['cseq']))
-
-    ntrnsent, ndevsent, ntstsent = list(map(len, (trn,dev,tst)))
-    logger.info('# of sents trn, dev, tst: {} {} {}'.format(ntrnsent, ndevsent, ntstsent))
-
-    MAX_LENGTH = max(len(sent['cseq']) for sent in chain(trn,dev,tst))
-    MIN_LENGTH = min(len(sent['cseq']) for sent in chain(trn,dev,tst))
-    AVG_LENGTH = np.mean([len(sent['cseq']) for sent in chain(trn,dev,tst)])
-    STD_LENGTH = np.std([len(sent['cseq']) for sent in chain(trn,dev,tst)])
-    logger.info('maxlen: {} minlen: {} avglen: {:.2f} stdlen: {:.2f}'.format(MAX_LENGTH, MIN_LENGTH, AVG_LENGTH, STD_LENGTH))
-
-    feat = featchar.Feat(args['feat'])
-    feat.fit(trn,dev,tst)
-
-    batcher = Batcher(args['n_batch'], feat)
-    reporter = Reporter(feat, rep.get_ts)
-
-    validator = Validator(trn, dev, tst, batcher, reporter) if args['curriculum'] < 2 else Curriculum(trn, dev, batcher, reporter, args['curriculum'])
-
-    # select rnn
-    RNN = RDNN
-
-    rnn_params = extract_rnn_params(args)
-    rdnn = RNN(feat.NC, feat.NF, args)
-    """ tprobs """ # TODO
-    tfollow = set()
-    for sent in trn:
-        tseq = feat.tseqenc.transform([t for t in sent['tseq']])
-        tfollow.update(set(zip(tseq,tseq[1:])))
-    tprobs = np.zeros((feat.NC,feat.NC),dtype=np.float32)
-    for i,j in tfollow:
-        tprobs[i,j] = 1
-    rdnn.tprobs = tprobs
-    """ end tprobs """
-    validator.validate(rdnn, args)
-    # lr: scipy.stats.expon.rvs(loc=0.0001,scale=0.1,size=100)
-    # norm: scipy.stats.expon.rvs(loc=0, scale=5,size=10)
 
 def objective(hargs):
     logger = logging.getLogger()
@@ -356,7 +219,6 @@ def objective(hargs):
     args = vars(parser.parse_args())
 
     args['lr'] = hargs['lr']
-    args['opt'] = hargs['opt']
     args['decoder'] = hargs['decoder']
     args['n_hidden'] = hargs['n_hidden']
     args['n_batch'] = hargs['n_batch']
@@ -365,15 +227,8 @@ def objective(hargs):
     args['fbias'] = hargs['fbias']
     args['emb'] = hargs['emb']
 
-    if hargs['layer'] == 1:
-        args['drates'] = [hargs['d1'], hargs['d2']]
-        args['n_hidden'] = [hargs['n_hidden']]
-    elif hargs['layer'] == 2:
-        args['drates'] = [hargs['d1'], hargs['d2'], hargs['d3']]
-        args['n_hidden'] = [hargs['n_hidden'], hargs['n_hidden']]
-    else:
-        args['drates'] = [hargs['d1'], hargs['d2'], hargs['d3'], hargs['d4']]
-        args['n_hidden'] = [hargs['n_hidden'], hargs['n_hidden'], hargs['n_hidden']]
+    args['drates'] = [hargs['d1'], hargs['d2'], hargs['d3'], hargs['d4']]
+    args['n_hidden'] = [hargs['n_hidden'], hargs['n_hidden'], hargs['n_hidden']]
 
     # print args
     for k,v in sorted(args.iteritems()):
@@ -487,12 +342,11 @@ if __name__ == '__main__':
     logger.addHandler(shandler);logger.addHandler(ihandler);logger.addHandler(dhandler);
 
     common = {}
-    common['lr'] = hp.choice('lr', [0.001, 0.001, 0.005, 0.01, 0.1, 1, 2])
-    common['opt'] = hp.choice('opt', ['adam', 'sgd'])
+    common['lr'] = hp.uniform('lr', 0.0001, 0.005)
     common['decoder'] = hp.choice('decoder', [0, 1])
     common['n_hidden'] = hp.choice('n_hidden', [64, 128, 256])
-    common['n_batch'] = hp.choice('n_batch', [32, 64, 128])
-    common['norm'] = hp.choice('norm', [0.01, 0.1, 1, 2, 5, 10])
+    common['n_batch'] = hp.choice('n_batch', [64, 128])
+    common['norm'] = hp.uniform('norm', 0.5, 1.5)
     common['shuf'] = hp.choice('shuf', [True, False])
     common['fbias'] = hp.uniform('fbias', 0, 2)
     common['emb'] = hp.choice('emb', [0, 64, 128, 256])
@@ -500,7 +354,6 @@ if __name__ == '__main__':
     common['d2'] = hp.uniform('d2', 0, 1)
     common['d3'] = hp.uniform('d3', 0, 1)
     common['d4'] = hp.uniform('d4', 0, 1)
-    common['layer'] = hp.choice('layer', [1,2,3])
 
     space = common
     trials = Trials()
