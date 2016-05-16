d49694b3064dc5fb61630bf0b91cbb7307af677f
Onur Kuru <kuru.onur1@gmail.com>
2016-05-16 11:01:33 +0300

    chunk->ner

--- src/xfer.py
+++ src/xfer.py
@@ -2,13 +2,14 @@ import numpy as np
 import argparse
 import random
 import lasagne
+import theano
 import logging
 
 from lazrnn import RDNN
 import rep, featchar, exper
 from exper import Batcher, Reporter, Validator, Dset
 from utils import get_sents
-import decoder
+import decoder, utils
 
 """
 [W, # for emb layer
@@ -54,6 +55,7 @@ def get_args():
     parser = argparse.ArgumentParser(prog="xfer")
     parser.add_argument('model_file')
     parser.add_argument('lang')
+    parser.add_argument('log')
     parser.add_argument("--breaktrn", default=0, type=int, help="break trn sents to subsents")
     parser.add_argument("--captrn", default=500, type=int, help="consider sents lt this as trn")
     parser.add_argument("--sorted", default=1, type=int, help="sort datasets before training and prediction")
@@ -74,9 +76,9 @@ def setup_logger(args):
     # param_log_name = ','.join(['{}:{}'.format(p,args[p]) for p in LPARAMS])
     # param_log_name = valid_file_name(param_log_name)
     base_log_name = '{}:{},{}'.format(host, theano.config.device, args['log'])
-    ihandler = logging.FileHandler('{}/{}.info'.format(LOG_DIR,base_log_name), mode='w')
+    ihandler = logging.FileHandler('{}/{}.info'.format(utils.LOG_DIR,base_log_name), mode='w')
     ihandler.setLevel(logging.INFO)
-    dhandler = logging.FileHandler('{}/{}.debug'.format(LOG_DIR,base_log_name), mode='w')
+    dhandler = logging.FileHandler('{}/{}.debug'.format(utils.LOG_DIR,base_log_name), mode='w')
     dhandler.setLevel(logging.DEBUG)
     logger.addHandler(shandler);logger.addHandler(ihandler);logger.addHandler(dhandler);
 
