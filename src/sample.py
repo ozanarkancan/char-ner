ea373141dda4df2582a096072b5a693f5154fc12
Onur Kuru <kuru.onur1@gmail.com>
2016-08-04 15:22:37 +0300

    sample

---
+++ src/sample.py
@@ -0,0 +1,42 @@
+import argparse, random, os
+from utils import get_sents
+
+def get_args():
+    parser = argparse.ArgumentParser(prog="sample")
+
+    parser.add_argument("dset", help="dataset")
+    parser.add_argument("nums", nargs='+', type=int, help="num of sents to sample from dataset for train, dev, tst (in the order of K)")
+    args = vars(parser.parse_args())
+
+    return args
+
+def write_to_file(fpath, dset_part):
+    with open(fpath,'w') as src:
+        for sent in dset_part:
+            for w,t in zip(sent['ws'], sent['ts']):
+                src.write(('%s\t%s\n'%(w,t)).encode('utf-8'))
+            src.write('\n')
+
+
+def get_sample(l, k):
+    rand_indices = random.sample(xrange(len(l)),k)
+    return [l[i] for i in rand_indices]
+
+if __name__ == '__main__':
+    random.seed(7)
+    args = get_args()
+    print args
+    trn, dev, tst = get_sents(args['dset'])
+    dset_parts = (trn,dev,tst)
+    print map(len, (trn,dev,tst))
+    strn, sdev, stst = map(get_sample, dset_parts, map(lambda x: x*1000, args['nums']))
+    print map(len, (strn,sdev,stst))
+
+    dir_name = 'data/%s-sample'%args['dset']
+    os.mkdir(dir_name)
+
+    for fname, dpart in zip(('train.bio','testa.bio', 'testb.bio'), (strn,sdev,stst)):
+        fpath = '%s/%s'%(dir_name,fname)
+        print fpath
+        write_to_file(fpath, dpart)
+
---
+++ src/tmp.py
@@ -0,0 +1,9 @@
+
+def a(a,b):
+    return 6
+
+def a(c):
+    return 5
+
+if __name__ == '__main__':
+    print a(5,6)
